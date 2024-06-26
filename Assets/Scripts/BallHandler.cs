b525a58c0beab358910992e8f34c229369507d23
ChrisG3D <59054870+ChrisG3D@users.noreply.github.com>
2024-06-26 16:40:43 +0200

    Add launch ball logic

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,11 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D ballRigidbody;
+    [SerializeField] private SpringJoint2D ballSpringJoint;
+    [SerializeField] private float detachDelay = 0.15f;
+
     private Camera mainCamera;
+    private bool isDragging = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +25,34 @@ public class BallHandler : MonoBehaviour
         // Out the method if not touching the screen
         if(!Touchscreen.current.primaryTouch.press.IsPressed())
         {
-            ballRigidbody.isKinematic = false;
+            if(isDragging)
+            {
+                LaunchBall();
+                isDragging = false;
+            }
+
             return;
         }
+
+        isDragging = true;
 
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
         ballRigidbody.isKinematic = true;
         ballRigidbody.position = worldPosition;
+    }
 
+    private void LaunchBall()
+    {
+        ballRigidbody.isKinematic = false;
+        ballRigidbody = null;
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        ballSpringJoint.enabled = false;
+        ballSpringJoint = null;
     }
 }
